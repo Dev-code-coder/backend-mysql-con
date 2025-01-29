399cc41b1aebe24507597b29ee7648605277b08b
Ragu <deena@tibura.com>
2025-01-29 06:33:06 +0530

    changes

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const router = require('./router/index');
+
+app.use(router);
+
+const msg = ($msg)=>{
+console.log($msg);
+}
+
+app.listen(PORT,msg(`Server is running as ${PORT}`))
